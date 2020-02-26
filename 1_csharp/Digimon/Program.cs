ffa7b8a8f3a0efb16fc9a8cd487fa4035b39f092
vhdelcampo <vhdelcampo@miners.utep.edu>
2020-02-26 11:29:23 -0600

    Add Hello World and Digimoin projects and changed gitignore

---
+++ 1_csharp/Digimon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Digimon
+{
+  internal class Digimon {}
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var p = new Program();
+      p.Fight(null, null);
+    }
+    public void Fight(Digimon a, Digimon b)
+    {
+      System.Console.WriteLine("{0} fights {1}", a, b);
+    }
+  }
+}
