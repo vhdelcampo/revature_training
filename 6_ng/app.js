bfdbdea8a9174e6244e4b082a7a4d354d56d45e5
victor <vhdelcampo@miners.utep.edu>
2020-03-31 16:21:31 -0500

    add javascript tutorial examples

---
+++ 6_ng/app.js
@@ -0,0 +1,41 @@
+// var x = 10;
+// let y = 10;
+// const z = 10;
+
+// let movie = {
+// title: "Back to the Future"
+// };
+
+// let movie2 = movie;
+
+// movie2.title = "Watchmen";
+
+// console.log(movie);
+
+// movie = new Movie();
+// movie2 = new Movie();
+// movie3 = Movie();
+
+// function Movie() {
+//     this.title = "Futurama";
+// }
+
+// movie.title = "Ugly Delicious";
+// movie2.title = "Yesterday";
+
+// let movie = new Movie();
+// let movie = new Movie();
+// let movie3 = Movie();
+
+// movie = Object.create(movie);
+// movie2 = Object.create(movie);
+
+// movie.title = "Fifth Element";
+// movie2.title = "Fast and Furious";
+
+// console.log(movie);
+// console.log(movoie2);
+
+console.log(message);
+
+let mesasage = "hello JS";
